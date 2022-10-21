d9a3cd305199274c973393485b63287c65d57b21
Antony Paquay <antony.paquay@gmail.com>
2022-10-21 08:36:45 +0200

    feat(): exists and delete methods

--- crm.py
+++ crm.py
@@ -1,11 +1,11 @@
 import re
 import string
-from tinydb import TinyDB, where
+from tinydb import TinyDB, where, table
 from pathlib import Path
+from typing import List
 
 
 class User:
-
     DB = TinyDB(Path(__file__).resolve().parent / "data.json", indent=4)
 
     def __init__(self, first_name: str, last_name: str, phone_number: str = "", address: str = ""):
@@ -25,7 +25,7 @@ class User:
         return f"{self.first_name} {self.last_name}"
 
     @property
-    def db_instance(self):
+    def db_instance(self) -> table.Document:
         return User.DB.get((where("first_name") == self.first_name) & (where("last_name") == self.last_name))
 
     def _checks(self):
@@ -35,22 +35,34 @@ class User:
     def _check_phone_number(self):
         phone_number = re.sub(r"[+()\s]*", "", self.phone_number)
         if len(phone_number) < 10 or not phone_number.isdigit():
-            raise ValueError(f"Phone number {phone_number} is invalid.")
+            raise ValueError(f"Phone number {self.phone_number} is invalid.")
 
     def _check_names(self):
         if not (self.first_name and self.last_name):
             raise ValueError("The first and last name cannot be empty.")
+
         special_characters = string.punctuation + string.digits
 
         for character in self.first_name + self.last_name:
             if character in special_characters:
                 raise ValueError(f"Invalid name {self.full_name}")
 
+    def exists(self):
+        return bool(self.db_instance)
+
+    def delete(self) -> List[int]:
+        if self.exists():
+            return User.DB.remove(doc_ids=[self.db_instance.doc_id])
+        return []
+
     def save(self, validate_data: bool = False) -> int:
         if validate_data:
             self._checks()
 
-        return User.DB.insert(self.__dict__)
+        if self.exists():
+            return -1
+        else:
+            return User.DB.insert(self.__dict__)
 
 
 def get_all_users():
@@ -58,15 +70,5 @@ def get_all_users():
 
 
 if __name__ == "__main__":
-    martin = User.db_instance('Manon', 'Gaillard')
-    print(martin)
-    # from faker import Faker
-    # fake = Faker(locale="fr_FR")
-
-    # for _ in range(5):
-    #     user = User(first_name=fake.first_name(),
-    #                 last_name=fake.last_name(),
-    #                 phone_number=fake.phone_number(),
-    #                 address=fake.address())
-    #     print(user.save(True))
-    #     print("-" * 10)
+    manon = User('Manon', 'Gaillard')
+    print(manon.delete())
